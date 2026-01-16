679c47e3a1ca1f198b22ecf0687b39e77e4be12c
DaudKhan-cell <daud12319@outlook.com>
2026-01-16 03:37:04 +0000

    01_basics config done

---
+++ 01_basics/variables1.js
@@ -0,0 +1,9 @@
+const accountId = "1234";
+let accountEmail = "hahaha@outlook.com";
+var accountPass = "34510";
+//Not we donot prefer var because of scope issue.
+// we cannot update const
+
+//const accountId = "256";
+
+console.table([accountEmail, accountId, accountPass]);
