ce450ab953039209d0a153a90d2505358c9810c4
DaudKhan-cell <daud12319@outlook.com>
2026-01-18 03:43:39 +0000

    datatypes1

---
+++ 01_basics/datatypes1.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all code as newer version of JavaScript
+//alert("Hello") //we are using node js not browser
+console.log(3+3);
+ let name = "Daud Khan"
+ let age = 18
+ let isLoggedIn = false
+
+ //number
+ //string
+ //boolean
+ //null
+ let state = null
+ //undefined
+ let x;
+ //symbol => unique
+ //object 
+
+ console.log(typeof age)
+ console.log(typeof null) //type of Object
+
